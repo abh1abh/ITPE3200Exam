2604af3930ab3a3cd106c129c03b77fa4be5d3c9
alexanderhoyskel <1664AlfredAnna>
2025-10-21 17:31:35 +0200

    Update remove .vscode config

--- api/Program.cs
+++ api/Program.cs
@@ -22,7 +22,6 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddSwaggerGen(c =>
 {
