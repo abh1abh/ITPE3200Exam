2882ab3dfc2be4ee3b91e10b3f158fe008ce52a9
abh1abh <alexhoyskel@yahoo.no>
2025-10-28 15:19:21 +0100

    Remove uneeded repo methods

--- api/DAL/IChangeLogRepository.cs
+++ api/DAL/IChangeLogRepository.cs
@@ -4,8 +4,6 @@ namespace HomecareAppointmentManagement.DAL;
 
 public interface IChangeLogRepository
 {
-    Task<IEnumerable<ChangeLog>?> GetAll();
-    Task<ChangeLog?> GetById(int id);
     Task<bool> Create(ChangeLog changeLog);
     Task<IEnumerable<ChangeLog>?> GetByAppointmentId(int appointmentId);
 }
