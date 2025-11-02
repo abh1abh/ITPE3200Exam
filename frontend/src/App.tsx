2dba7672c4e55579f5f65f38e9c24a8cfb26c1e1
laros019 <laros0774@oslomet.no>
2025-11-02 21:59:02 +0100

    Added authentication system with login and registration pages, including admin registration  functionality.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,28 @@ import HomePage from "./home/HomePage";
 import Footer from "./shared/Footer";
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import TeamPage from "./dummypages/TeamPage";
+import LoginPage from "./auth/LoginPage";
+import RegisterPage from "./auth/RegisterPage";
+import { AuthProvider } from "./auth/AuthContext";
+import RegisterAdmin from "./auth/RegisterAdmin";
 
 function App() {
   return (
+    <AuthProvider>
     <div>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/footer" element={<Footer />} />
           <Route path="/team" element={<TeamPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/admin/register" element={<RegisterAdmin />} />
         </Routes>
         <Footer />
       </Router>
     </div>
+    </AuthProvider>
   );
 }
 
