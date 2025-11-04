ff90eed18c1d8ccf1ca07eae4ee83a5cec5cb18d
abh1abh <alexhoyskel@yahoo.no>
2025-11-04 14:01:35 +0100

    Added Role route

--- frontend/src/auth/ProtectedRoute.tsx
+++ frontend/src/auth/ProtectedRoute.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from './AuthContext';
+import React from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+import Loading from "../shared/Loading";
 
 const ProtectedRoute: React.FC = () => {
-    const { token } = useAuth();
+  const { token, isLoading } = useAuth();
+  const location = useLocation();
 
-    if (!token) {
-        // Redirect them to the /login page, but save the current location they were
-        // trying to go to. This allows us to send them along to that page after they login.
-        return <Navigate to="/login" replace />;
-    }
+  if (isLoading) return <Loading />;
 
-    return <Outlet />;
+  if (!token) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to. This allows us to send them along to that page after they login.
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
