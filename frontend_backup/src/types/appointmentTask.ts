049ed95062b2c3f9340fdb1aaaeac54a897f0bec
Onat <onuce0978@oslomet.no>
2025-11-06 10:34:02 +0100

    (cleanup)

--- frontend_backup/src/types/appointmentTask.ts
+++
@@ -1,7 +0,0 @@
-/*export interface AppointmentTaskDto {
-  id?: number;
-  appointmentId?: number;
-  description: string;
-  isCompleted: boolean;
-}
-  */
--- frontend_backup9999/frontend_/src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-export interface LoginDto {
-username: string;
-password: string;
-}
-export interface RegisterDto {
-username: string;
-email: string;
-password: string;
-}
